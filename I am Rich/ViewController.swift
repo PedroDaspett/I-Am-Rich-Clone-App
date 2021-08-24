6a8aa489d77fab91b2e010991489139d8e856284
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2021-08-24 08:34:47 -0300

    Adding app files

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Pedro Couventaris Daspett on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
